6b5a1547df16d010173922e7043a1e2f33aaeffa
Pham Qui Duong <phamquiduong@outlook.com>
2023-09-11 22:45:15 +0700

    Change app core name

--- app_core/urls.py
+++ app_core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for core_tkp_quiz project.
+URL configuration for app_core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
