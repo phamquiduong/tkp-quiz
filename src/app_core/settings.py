a42a1b167d879a8bf0a97237d806d10c93f8a4fc
Duong Pham Qui <duongpq@nal.vn>
2023-09-14 10:16:55 +0700

    Add docker build

--- src/app_core/settings.py
+++ src/app_core/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = 'app_core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+DB_FOLDER = BASE_DIR / '../.database/'
+DB_FOLDER.mkdir(parents=True, exist_ok=True)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../db.sqlite3',
+        'NAME': DB_FOLDER / 'db.sqlite3',
     }
 }
 
@@ -125,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../.static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
