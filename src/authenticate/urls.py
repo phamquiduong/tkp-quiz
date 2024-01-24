00af39fb38329102a5b161c281330df308c51f31
Pham Qui Duong <phamquiduong@outlook.com>
2024-01-25 00:48:36 +0700

    Register user when first start website

--- src/authenticate/urls.py
+++ src/authenticate/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from authenticate.views import home_view
+from authenticate.views import home_view, register_view
 
 urlpatterns = [
     path('', home_view, name='auth_home'),
+    path('register', register_view, name='auth_register'),
     path('login', auth_views.LoginView.as_view(template_name="authenticate/login.html"), name='auth_login'),
     path('logout', auth_views.LogoutView.as_view(next_page='auth_home'), name='auth_logout'),
 ]
--- src/authenticate/views.py
+++ src/authenticate/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import redirect
-from django.views.decorators.http import require_GET
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_GET, require_POST
+
+User = get_user_model()
 
 
 @require_GET
 def home_view(request):
+    if not User.objects.all().exists():
+        return render(request, 'authenticate/register.html')
+
     if not request.user.is_authenticated:
         return redirect('auth_login')
 
@@ -11,3 +17,17 @@ def home_view(request):
         return redirect('teacher_class_room__list_create')
 
     return redirect('student_home')
+
+
+@require_POST
+def register_view(request):
+    if User.objects.all().exists():
+        return redirect('auth_home')
+
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    user = User.objects.create_superuser(email=email, password=password)   # type: ignore
+    login(request, user)
+
+    return redirect('auth_home')
