2428df4654c4f5c899a1a500c41fa0e65746ed4a
Pham Qui Duong <phamquiduong@outlook.com>
2024-01-25 00:53:45 +0700

    Remove redundant character of bat

--- run_server.bat
+++ run_server.bat
@@ -1,21 +1,18 @@
 @echo off
 
-echo "----------------------------------------------------"
-echo "            Install all pip packages"
+echo ----------------------------------------------------
+echo             Install all pip packages
 pip install -r requirements.txt
-echo "\n"
 
-echo "----------------------------------------------------"
-echo "     Change directory to source code directory"
+echo ----------------------------------------------------
+echo      Change directory to source code directory
 cd src
-echo "\n"
 
-echo "----------------------------------------------------"
-echo "                  Run migrate"
+echo ----------------------------------------------------
+echo                   Run migrate
 python manage.py migrate
-echo "\n"
 
-echo "----------------------------------------------------"
-echo "                   Run server"
+echo ----------------------------------------------------
+echo                    Run server
 set /p port="Please Input Server Port: "
 python manage.py runserver 0.0.0.0:%port% --insecure
